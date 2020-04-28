3fce24b02dc48e306304c9f7a8f6772d2c17ba7e
vlad <vlad.lychagin@gmail.com>
2020-04-28 17:59:41 +0300

    Первая версия проекта

---
+++ EnvironmentInfo/EnvironmentInfo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnvironmentInfo
+{
+    class Program
+    {
+        static void Main(string[] args) {
+            Console.WriteLine("Привет, мир!");
+            Console.WriteLine($"Компьютер: {Environment.MachineName}, версия ОС: {Environment.OSVersion}");
+
+            Console.WriteLine($"{(new DriveInfo("c:").TotalFreeSpace) / 1024 / 1024:n0} МБайт");
+            Console.Write("\n\n\t");
+        }
+    }
+}
